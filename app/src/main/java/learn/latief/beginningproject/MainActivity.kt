d932e662d9ab7426e5e48ae40a601040ea7c5169
m0zl4 <zayuka040104@gmail.com>
2021-09-16 11:36:36 +0700

    Fix Progress Bar

--- app/src/main/java/learn/latief/beginningproject/MainActivity.kt
+++ app/src/main/java/learn/latief/beginningproject/MainActivity.kt
@@ -1,39 +1,69 @@
 package learn.latief.beginningproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var dataEmail : Array<String>
     private lateinit var dataPassword : Array<String>
+    private var progressBar: ProgressBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val edtEmail : EditText = findViewById(R.id.edit_email)
-        val edtPass :EditText = findViewById(R.id.edit_password)
+        val edtPassword : EditText = findViewById(R.id.edit_password)
         val btnLogin : Button = findViewById(R.id.btnLogin)
-        val btnSign : Button = findViewById(R.id.btnSign)
-        val btnForget : TextView = findViewById(R.id.forget)
-        val btnRemember : RadioButton = findViewById(R.id.remember)
+        val btnRegister : Button = findViewById(R.id.btnSign)
+        progressBar = findViewById(R.id.progressbar)
+        val btnForgetPass : TextView = findViewById(R.id.forget)
+        val radioRemember : RadioButton = findViewById(R.id.remember)
+
+
+//        val builder = AlertDialog.Builder(this, R.style.AlertDialog_AppCompat).create()
+//        builder.setTitle("Harap isi")
+//        builder.setMessage("Tentukan pilihan anda!")
+//        builder.setIcon(R.drawable.ic_person)
+//        builder.setCancelable(true)
+//        builder.show()
 
         dataEmail = arrayOf("Muhammad", "Abdul", "Latief")
-        dataPassword = arrayOf("1234", "1710", "2004")
+        dataPassword = arrayOf("1234", "4321", "1423")
+
+        btnLogin.setOnClickListener {
+            progressBar?.visibility = View.VISIBLE
+            signIn(edtEmail.text.toString(), edtPassword.text.toString())
+        }
+
+        btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
 
     }
 
-    fun signIn(email : String, password: String){
+    fun signIn(email: String, password: String)
+    {
         for (i in dataEmail.indices){
             val takeDataEmail = dataEmail.get(i)
             val takeDataPass = dataPassword.get(i)
-            if (takeDataEmail == email || takeDataPass == password){
-                Toast.makeText(this, "LogedIn", Toast.LENGTH_SHORT ).show()
+            if (takeDataEmail == email && takeDataPass == password){
+                Toast.makeText(this, "LogedIn", Toast.LENGTH_SHORT).show()
+                progressBar?.visibility = View.GONE
+                startActivity(Intent(this, HomeActivity::class.java))
+                break
             }else{
-                Toast.makeText(this, "Wrong Account", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Akun Slaha", Toast.LENGTH_SHORT).show()
+                progressBar?.visibility = View.GONE
             }
         }
     }
 }
+
